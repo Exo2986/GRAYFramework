9cb58e981229a4f08352f89a321ddf9e05afb125
ExoMaster <exomaster2986@gmail.com>
2018-07-03 17:25:53 -0400

    Update version 1.0.1

--- src/main/java/exomaster/grayfw/Util.java
+++ src/main/java/exomaster/grayfw/Util.java
@@ -4,12 +4,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Util {
+    protected static String programID = "EXAMPLE";
+
+    public static String getProgramID() {
+        return programID;
+    }
+
+    public static void setProgramID(String newID) {
+        programID = newID;
+    }
 
     public static void consoleLog(Object... params) {
         StringBuilder sb = new StringBuilder();
         for (Object o : params)
             sb.append(o).append(' ');
-        System.out.println('(' + new SimpleDateFormat("MM/dd/yy HH:mm:ss z").format(new Date()) + ") GRAY: " + sb.toString().trim());
+        System.out.println('(' + new SimpleDateFormat("MM/dd/yy HH:mm:ss z").format(new Date()) + ") " + programID + ": " + sb.toString().trim());
     }
 
 }
